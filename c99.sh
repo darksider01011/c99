11846d3f9442f48b34ba7832a9c6d235990be81f
mostafa taghizadeh <132552898+dark01101@users.noreply.github.com>
2023-08-17 16:38:18 +0100

    Update c99.sh

--- c99.sh
+++ c99.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 echo ""
 echo "EXTRACT SUBDOMAIN FROM subdomainfinder.c99.nl WITHOUT API KEY :)"
 echo ""
@@ -10,14 +9,20 @@ month=$(date +%m)
 input="day.txt"
 while IFS= read -r line
 do
-  sleep 1
+sleep 1
   echo "$line"
 curl https://subdomainfinder.c99.nl/scans/2023-$month-$line/$1.$2 > $line.txt
 done < "$input"
 
-cat 1.txt 2.txt 3.txt 4.txt 5.txt 6.txt 7.txt 8.txt 9.txt 10.txt 11.txt 12.txt 13.txt 14.txt 15.txt 16.txt 17.txt 18.txt 19.txt 20.txt 21.txt 22.txt 23.txt 24.txt 25.txt 26.txt 27.txt 28.txt 29.txt 30.txt 31.txt >> do.txt
+clear
 
-rm -rf 1.txt 2.txt 3.txt 4.txt 5.txt 6.txt 7.txt 8.txt 9.txt 10.txt 11.txt 12.txt 13.txt 14.txt 15.txt 16.txt 17.txt 18.txt 19.txt 20.txt 21.txt 22.txt 23.txt 24.txt 25.txt 26.txt 27.txt 28.txt 29.txt 30.txt 31.txt
+echo ""
+echo "PLEASE WAIT..."
+echo ""
+
+cat 1.txt 2.txt 3.txt 4.txt 5.txt 6.txt 7.txt 8.txt 9.txt  10.txt 11.txt 12.txt 13.txt 14.txt 15.txt 16.txt 17.txt 18.txt 19.txt 20.txt 21.txt 22.txt 23.txt 24.txt 25.txt 26.txt 27.txt 28.txt 29.txt 30.txt 31.txt >> do.txt
+
+rm -rf 1.txt 2.txt 3.txt 4.txt 5.txt 6.txt 7.txt 8.txt 9.txt  10.txt 11.txt 12.txt 13.txt 14.txt 15.txt 16.txt 17.txt 18.txt 19.txt 20.txt 21.txt 22.txt 23.txt 24.txt 25.txt 26.txt 27.txt 28.txt 29.txt 30.txt 31.txt
 
 grep -Po '([a-z0-9][a-z0-9\-]{0,61}[a-z0-9]\.)+[a-z0-9][a-z0-9\-]*[a-z0-9]' do.txt > 1.txt
 
@@ -29,9 +34,7 @@ grep  -Po "(^|^[^:]+:\/\/|[^\.].+\.)$1\.$2"  2.txt > 3.txt
 
 rm -rf 2.txt
 
-
 clear
-
 
 echo ""
 echo "RESULTS :"
